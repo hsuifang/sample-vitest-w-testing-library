647188e550c340ae2cd4d94a37433d58e46f71e1
Evonne Hsu <evonne.hsu@kenkone.com>
2023-11-23 17:40:27 +0800

    Add spyon

--- src/hooks/useMovies.spec.ts
+++ src/hooks/useMovies.spec.ts
@@ -2,16 +2,27 @@ import { renderHook, waitFor } from "@testing-library/react";
 import { useMovies } from "./useMovies";
 
 describe("useMovies", () => {
+  const fetchSpy = vi.spyOn(global, "fetch");
+  beforeAll(() => {
+    const mockResolveValue = {
+      ok: true,
+      json: () =>
+        new Promise((resolve) => {
+          resolve({ results: [{ title: "Star Wars" }] });
+        }),
+    };
+    fetchSpy.mockReturnValue(mockResolveValue as any);
+  });
+
+  afterAll(() => {
+    fetchSpy.mockRestore();
+  });
+
   it("should fetch movies", async () => {
     const { result } = renderHook(() => useMovies());
 
-    await waitFor(
-      () => {
-        expect(result.current.movies).toEqual([{ title: "Star Wars" }]);
-      },
-      {
-        timeout: 2000,
-      }
-    );
+    await waitFor(() => {
+      expect(result.current.movies).toEqual([{ title: "Star Wars" }]);
+    });
   });
 });
--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -13,18 +13,24 @@ export const useMovies = (): {
   const [isLoading, setIsLoading] = useState(false);
   const [moviesError, setMoviesError] = useState<string | null>(null);
 
-  const service = (): Promise<{ movies: Movie[] }> =>
-    new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({ movies: [{ title: "Star Wars" }] });
-      }, 1000);
-    });
+  // const service = (): Promise<{ movies: Movie[] }> =>
+  //   new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve({ movies: [{ title: "Star Wars" }] });
+  //     }, 1000);
+  //   });
 
   const fetchMovies = async () => {
     try {
       setIsLoading(true);
-      const res = await service();
-      setMovies(res.movies);
+      const response = await fetch("https://swapi.dev/api/films");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+      setMovies(data.results);
       setIsLoading(false);
     } catch (error: unknown) {
       if (error instanceof Error) {
